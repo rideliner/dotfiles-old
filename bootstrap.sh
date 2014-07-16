c041a3de1fa6fca45996420eba9c6e0aa0673049
Nathan Currier <nathan.currier@gmail.com>
2014-07-16 16:35:07 -0600

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,83 @@
 #!/usr/bin/env zsh
 
-function symlink_dotfiles {
-  local REAL_PATH="${$(readlink -fnq $0)%/*}"
+function user() {
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
 
-  local i patched
-  for i in **/*.symlink(n) ; do
-    patched=".${${i##*/}%\.*}"
+function success() {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
 
-    if [[ -d $i ]]; then
-      ln -fns "${REAL_PATH}${DOTFILES}/${i}" "$HOME/$patched"
-      echo "${REAL_PATH}${DOTFILES}/${i}\t$HOME/$patched"
-    else
-      ln -fs "${REAL_PATH}${DOTFILES}/${i}" "$HOME/$patched"
-      echo "${REAL_PATH}${DOTFILES}/${i}\t$HOME/$patched"
+function link_file() {
+  local src=$1 dst=$2
+
+  local overwrite backup skip
+  local action
+
+  if [[ -f "$dst" || -d "$dst" || -L "$dst" ]]; then
+    if [[ "$overwrite_all" == "false" && "$backup_all" == "false" &&  "$skip_all" == "false" ]]; then
+      local currentSrc="$(readlink $dst)"
+
+      if [[ "$currentSrc" == "$src" ]]; then
+        skip="true"
+      else
+        user "File already exists: $(basename "$dst"), what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -s -k 1 "action"
+
+        case "$action" in
+          o )
+            overwrite="true";;
+          O )
+            overwrite_all="true";;
+          b )
+            backup="true";;
+          B )
+            backup_all="true";;
+          s )
+            skip="true";;
+          S )
+            skip_all="true";;
+          * )
+            ;;
+        esac
+      fi
     fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [[ "$overwrite" == "true" ]]; then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [[ "$backup" == "true" ]]; then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [[ "$skip" == "true" ]]; then
+      success "skipped $src"
+    fi
+  fi
+
+  if [[ "$skip" != "true" ]]; then  # "false" or empty
+    ln -s "$src" "$dst"
+    success "linked $src to $dst"
+  fi
+}
+
+function symlink_dotfiles() {
+  local REAL_PATH="${$(readlink -fnq $0)%/*}"
+  local src dst
+  local overwrite_all=false backup_all=false skip_all=false
+
+  for src in **/*.symlink(n) ; do
+    dst="$HOME/.$(basename "${src%\.*}")"
+    src="${REAL_PATH}/${src}"
+
+    link_file "$src" "$dst"
   done
 }
 
