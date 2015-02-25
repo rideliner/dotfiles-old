71b519c5304f2b5710e8312c52a59661c38c19de
Nathan Currier <nathan.currier@gmail.com>
2015-02-25 15:43:18 -0700

    Split help message into a .help file at the root of the dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,53 +4,6 @@ source "${0:A:h}/.internal/path.zsh"
 source "$DOTFILES_PATH/.internal/mode.zsh"
 source "$DOTFILES_PATH/.internal/dependency.zsh"
 
-function dotfiles() {
-  local mods
-
-  getModulesAndDependencies mods
-  loadMeta mods
-
-  for src ($DOTFILES_PATH/.internal/*.bootstrap(N) $DOTFILES_PATH/${^mods}/*.bootstrap(N)); do
-    source "$src"
-
-    status $? "run $src"
-  done
-}
-
-function help() {
-  if [[ $# -eq 0 ]]; then
-    echo "Modules need to be set in ~/.dot"
-    echo
-    echo "Format:"
-    echo "  zstyle ':ride' modules <mods>..."
-    echo "ex. zstyle ':ride' modules tmux terminal rvm ruby"
-    echo
-    echo "Available modules:"
-
-    local mods
-    findAllModules mods
-
-    for mod in $mods ; do
-      echo "  $mod"
-    done
-  else
-    local mod file
-
-    for mod (${(s:,:)*//=/}); do
-      file="$DOTFILES_PATH/$mod/.help"
-      if [[ -d $file:h ]]; then
-        if [[ -f $file ]]; then
-          source $file
-        else
-          warning "Module '$mod' does not have a help file"
-        fi
-      else
-        fail "'$mod' is not a module"
-      fi
-    done
-  fi
-}
-
 source ~/.dot
 
 zparseopts -D -E -A Args -- -help:: -usage
@@ -58,7 +11,20 @@ zparseopts -D -E -A Args -- -help:: -usage
 if (( ${+Args[--usage]} )); then
   echo "Usage: $0 [--usage|--help[=<module>[,<module>]*]]"
 elif (( ${+Args[--help]} )); then
-  help ${Args[--help]}
+  source "$DOTFILES_PATH/.help" ${Args[--help]}
 else
-  dotfiles
+  function {
+    local mods src
+
+    getModulesAndDependencies mods
+    loadMeta mods
+
+    mods=(.internal $mods)
+
+    for src ($DOTFILES_PATH/${^mods}/*.bootstrap(N)); do
+      source "$src"
+
+      status $? "run $src"
+    done
+  }
 fi
