6723f5d776b19ef1d1a63f442f6b455dedfbb8c0
Nathan Currier <nathan.currier@gmail.com>
2015-07-05 22:31:23 -0600

    Don't show the motd twice if .hushlogin doesn't already exist.

--- hush/motd.zsh
+++ hush/motd.zsh
@@ -3,7 +3,9 @@ function() {
   local md5
   md5=$(md5sum -t /etc/motd | cut -f1 -d' ')
   if ! cmp -s ~/.hushlogin =(echo $md5); then
+    if [[ -e ~/.hushlogin ]]; then
+      cat /etc/motd
+    fi
     echo "$md5" >| ~/.hushlogin
-    cat /etc/motd
   fi
 }
