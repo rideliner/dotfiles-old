786deef92a7789c75d2c571f39348dd01850ae8d
Nathan Currier <nathan.currier@gmail.com>
2015-08-10 00:45:23 -0600

    SSL env now resolves symlink lingo.

--- ssl/env.zsh
+++ ssl/env.zsh
@@ -1,10 +1,15 @@
 if [[ $UID != 0 ]]; then
+  source "$DOTFILES_PATH/.internal/resolve.zsh"
+
   function() {
     typeset -UTx SSL_CERT_DIR ssl_cert_dir
 
     local ssl_path
     zstyle -s ':ride:ssl:symlink:*' path ssl_path
 
-    ssl_cert_dir+=($ssl_path)
+    if [[ ! -z "$ssl_path" ]]; then
+      dotfileSymlinkResolve ssl_path 'path' 'ssl'
+      ssl_cert_dir+=($ssl_path)
+    fi
   }
 fi
