d7dd7a65c4e121f902bf10033f1e4364f1d408a2
Nathan Currier <nathan.currier@gmail.com>
2015-08-10 00:26:43 -0600

    Allow symlink resolving to be used from any bootstrap process.

---
+++ .internal/resolve.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+# Arguments:
+# 1- name of variable to store result
+# 2- name of symlink file
+# 3- name of module
+function dotfileSymlinkResolve() {
+  eval "$1=\"${${${(P)1/\?/$2}/\!/$3}/\~/$HOME}\""
+}
