97d5df8fa579223e72bd9f8b972fe79e457a8f1b
Nathan Currier <nathan.currier@gmail.com>
2015-10-23 10:54:26 -0600

    Add suspend capabilities for cinnamon screensavers. Add placeholders for screensaver now options.

--- cinnamon/screensaver.zsh
+++ cinnamon/screensaver.zsh
@@ -2,8 +2,18 @@ function lock() {
   if [[ "$1" == "off" ]]; then
     gsettings set org.cinnamon.desktop.session idle-delay 0
     gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-ac 0
+  elif [[ "$1" == "now" ]]; then
   else
     gsettings set org.cinnamon.desktop.session idle-delay 900
     gsettings set org.cinnamon.settings-daemon.plugins.power sleep-display-ac 900
+  fi
+}
+
+function suspend() {
+  if [[ "$1" == "off" ]]; then
+    gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+  elif [[ "$1" == "now" ]]; then
+  else
+    gsettings set org.cinnamon.settings-daemon.plugins.power sleep-inactive-ac-timeout 3600
   fi
 }
