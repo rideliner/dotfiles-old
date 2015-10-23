381e76b2e18e840c1f410f15185310b229c9416c
Nathan Currier <nathan.currier@gmail.com>
2015-10-23 14:22:34 -0600

    Add Pacman commands to see an up-to-date mirrorlist and to set it.

--- pacman/aliases.zsh
+++ pacman/aliases.zsh
@@ -1,2 +1,5 @@
 
 (( $+commands[yaourt] && $UID != 0 )) && alias pacman='yaourt'
+
+alias pacman-mirrors="curl -Ls -o - https://www.archlinux.org/mirrorlist/\?country\=US\&protocol\=http\&protocol\=https\&ip_version\=4\&ip_version\=6\&use_mirror_status\=on | sed -e 's/^#//' | grep -Pzo --color=never '# Score: [[:digit:]]\..*\nServer.*\n'"
+alias pacman-mirrors-update="pacman-mirrors | sudo tee /etc/pacman.d/mirrorlist >/dev/null"
