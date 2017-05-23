5f7cef26fdc5f3f39479ad79d4d6597ef1d8c4b0
Nathan Currier <nathan.currier@gmail.com>
2017-05-23 15:43:39 -0400

    Remove code duplication and don't print if a command is missing

--- terminal/languages.zsh
+++ terminal/languages.zsh
@@ -8,54 +8,60 @@ function languages() {
   echo "Python:"; languages/python "\t"
 }
 
-function languages/ruby/impl() {
+function languages/impl() {
   if (( $+commands[$1] )); then
-    echo "$2$($1 --version | cut -d' ' -f -2) $([[ $1 == $DOTFILES_RUBY || */$1 == $DOTFILES_RUBY ]] && echo "(*)")"
+    echo "$3$(languages/$2/impl "$commands[$1]")"
   fi
+}
+
+function languages/ruby/impl() {
+  echo "$($1 --version | cut -d' ' -f -2)$([[ $1 == $DOTFILES_RUBY || $1 == */$DOTFILES_RUBY ]] && echo " (*)")"
 }
 
 function languages/ruby() {
-  languages/ruby/impl mri $1
-  languages/ruby/impl rbx $1
+  languages/impl mri ruby $1
+  languages/impl rbx ruby $1
 }
 
 function languages/c/impl() {
-  if (( $+commands[$1] )); then
-    echo "$2$($1 --version | head -n1 | cut -d' ' -f1,3) $([[ $CC == $1 || $CC == */$1 ]] && echo "(CC)")"
-  fi
+  echo "$($1 --version | head -n1 | cut -d' ' -f1,3) $([[ $1 == $CC || $1 == */$CC ]] && echo "(CC)")"
 }
 
 function languages/c() {
-  languages/c/impl clang $1
-  languages/c/impl gcc $1
+  languages/impl clang c $1
+  languages/impl gcc c $1
 }
 
 function languages/c++/impl() {
-  if (( $+commands[$1] )); then
-    echo "$2$($1 --version | head -n1 | cut -d' ' -f1,3) $([[ $CXX == $1 || $CXX == */$1 ]] && echo "(CXX)")"
-  fi
+  echo "$($1 --version | head -n1 | cut -d' ' -f1,3) $([[ $1 == $CXX || $1 == */$CXX ]] && echo "(CXX)")"
 }
 
 function languages/c++() {
-  languages/c++/impl clang++ $1
-  languages/c++/impl g++ $1
+  languages/impl clang++ c++ $1
+  languages/impl g++ c++ $1
+}
+
+function languages/go/impl() {
+  echo "$($1 version | cut -d' ' -f3 | sed "s/go/go /")"
 }
 
 function languages/go() {
-  echo "$1$(go version | cut -d' ' -f3 | sed "s/go/go /")"
+  languages/impl go go $1
+}
+
+function languages/java/impl() {
+  echo "$($1 -version 2>&1 | head -n1 | cut -d' ' -f1,3 | sed "s/\"//g")"
 }
 
 function languages/java() {
-  echo "$1$(java -version 2>&1 | head -n1 | cut -d ' ' -f1,3 | sed "s/\"//g")"
+  languages/impl java java $1
 }
 
 function languages/python/impl() {
-  if (( $+commands[$1] )); then
-    echo "$2$($1 --version 2>&1 | sed "s/P/p/") $([[ ${commands[$1]:A} == ${commands[python]:A} ]] && echo "(*)")"
-  fi
+  echo "$($1 --version 2>&1 | sed "s/P/p/") $([[ ${1:A} == ${commands[python]:A} ]] && echo "(*)")"
 }
 
 function languages/python() {
-  languages/python/impl python2 $1
-  languages/python/impl python3 $1
+  languages/impl python2 python $1
+  languages/impl python3 python $1
 }
