7b750ebcd815b50171f8eb2bbf854e90952b83c3
Nathan Currier <nathan.currier@gmail.com>
2017-08-01 10:36:49 -0400

    Use SSH config for scp

--- ssh/alias.zsh
+++ ssh/alias.zsh
@@ -1,3 +1,4 @@
 
 alias ssh='ssh -F =(cat /dev/null ~/.ssh/config(N) ~/.ssh.config(N))'
+alias scp='scp -F =(cat /dev/null ~/.ssh/config(N) ~/.ssh.config(N))'
 alias sftp='sftp -F =(cat /dev/null ~/.ssh/config(N) ~/.ssh.config(N))'
